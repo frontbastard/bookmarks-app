1910c93910820bffb0e4dca5ba896b1196516caf
pavlo <frontbastard@gmail.com>
2024-12-04 23:51:50 +0100

    Create profile on authentication via Google

--- account/authentication.py
+++ account/authentication.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.models import User
 
+from account.models import Profile
+
 
 class EmailAuthBackend:
     """
     Authenticate using an e-mail address.
     """
+
     def authenticate(self, request, username=None, password=None):
         try:
             user = User.objects.get(email=username)
@@ -21,3 +24,10 @@ class EmailAuthBackend:
             return User.objects.get(pk=user_id)
         except User.DoesNotExist:
             return None
+
+
+def create_profile(backend, user, *args, **kwargs):
+    """
+    Create user profile for social authentication
+    """
+    Profile.objects.create(user=user)
