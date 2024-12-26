48dd0aaca14b53a19c16aa039a96165352ae810a
pavlo <frontbastard@gmail.com>
2024-12-26 11:44:57 +0100

    Store image view in Redis

--- images/views.py
+++ images/views.py
@@ -1,3 +1,5 @@
+import redis
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
@@ -8,6 +10,12 @@ from django.views.decorators.http import require_POST
 from actions.utils import create_action
 from .forms import ImageCreateForm
 from .models import Image
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+)
 
 
 @login_required
@@ -39,10 +47,15 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image:{image.id}:views")
     return render(
         request,
         "images/image/detail.html",
-        {"section": "images", "image": image}
+        {
+            "section": "images",
+            "image": image,
+            "total_views": total_views,
+        }
     )
 
 
